9a154e502db555dacfbea40da030628c632b581e
ArmandoDepaul <43102318+ArmandoDepaul@users.noreply.github.com>
2018-10-14 21:55:00 -0500

    Delete test.py

--- Lab4/test.py
+++
@@ -1,9 +0,0 @@
-from wsgiref.simple_server import make_server
-
-from sqlitezero import select_artists 
-
-with make_server('', 8000, select_artists) as httpd:
-    print("Serving on port 8000...")
-
-    # Serve until process is killed
-    httpd.serve_forever()
